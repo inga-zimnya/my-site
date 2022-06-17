18769dc0f1f5b292f24940a4fd0d676fbaef6b3d
Inga <engaezik@gmail.com>
2022-06-17 15:33:25 +0300

    updated

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,44 @@
 import resolve from '@rollup/plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
+import {terser} from 'rollup-plugin-terser';
 import replace from '@rollup/plugin-replace';
 import dotenv from 'dotenv';
 import typescript from 'rollup-plugin-typescript2';
 
 dotenv.config();
 
-export default {
-    input: 'src/main.ts',
-    output: {
-        file: 'bundle.js',
+export default [
+    {
+        input: 'src/main.ts',
+        output: {
+            file: 'bundle.js',
+        },
+        plugins: [
+            resolve({browser: true}),
+            typescript(),
+            replace({
+                values: {
+                    'process.env.SERVER_URL': JSON.stringify(process.env.SERVER_URL),
+                },
+                preventAssignment: true,
+            }),
+            // terser(),
+        ],
     },
-    plugins: [
-        resolve({ browser: true }),
-        typescript(),
-        replace({
-            values: {
-                'process.env.SERVER_URL': JSON.stringify(process.env.SERVER_URL),
-            },
-            preventAssignment: true,
-        }),
-        // terser(),
-    ],
-};
+    {
+        input: 'src/email.ts',
+        output: {
+            file: 'email_bundle.js',
+        },
+        plugins: [
+            resolve({browser: true}),
+            typescript(),
+            replace({
+                values: {
+                    'process.env.SERVER_URL': JSON.stringify(process.env.SERVER_URL),
+                },
+                preventAssignment: true,
+            }),
+            // terser(),
+        ],
+    }
+    ];
